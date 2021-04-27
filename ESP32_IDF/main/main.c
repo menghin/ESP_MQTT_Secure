06da662b433b859419bebcbe202dbce1f78e6a16
menghin <noreply@no.com>
2021-04-27 19:40:43 +0200

    Add battery level check to main routine and go to a long sleep if the battery level is low.

--- ESP32_IDF/main/main.c
+++ ESP32_IDF/main/main.c
@@ -12,7 +12,6 @@
 #include "driver/gpio.h"
 #include "driver/adc.h"
 
-
 #include <mqtt_sensor_wifi.h>
 #include <mqtt_sensor_data.h>
 #include <mqtt_sensor_bme680.h>
@@ -45,11 +44,12 @@ const char *MQTT_PASS = "pasword"; // leave blank if no credentials used
 
 static const char *TAG = "main";
 
-#define NO_OF_SAMPLES   64          //Multisampling
-static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1
-static const adc_atten_t atten = ADC_ATTEN_DB_0;
+#define NO_OF_SAMPLES 64                            //Multisampling
+static const adc_channel_t channel = ADC_CHANNEL_6; //GPIO34 if ADC1
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
 
-static float readBatteryLevel(){
+static float readBatteryLevel()
+{
 
     float batteryLevel = 0;
     uint32_t adc_reading = 0;
@@ -58,7 +58,8 @@ static float readBatteryLevel(){
     adc1_config_channel_atten(channel, atten);
 
     //Multisampling
-    for (int i = 0; i < NO_OF_SAMPLES; i++) {
+    for (int i = 0; i < NO_OF_SAMPLES; i++)
+    {
         adc_reading += adc1_get_raw((adc1_channel_t)channel);
     }
     adc_reading /= NO_OF_SAMPLES;
@@ -87,69 +88,79 @@ void app_main(void)
     struct timeval enter_app_main_time;
     struct timeval leave_app_main_time;
     int app_main_duration_ms;
-    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("*", ESP_LOG_WARN);
     gettimeofday(&enter_app_main_time, NULL);
     const int wakeup_time_sec = WAKEUP_TIME_SEC;
     ESP_LOGI(TAG, "Enabling timer wakeup, %d s", wakeup_time_sec);
-    esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000);
 
     // Get the batteryLevel
     float batteryLevel = readBatteryLevel();
 
-    // Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    if (batteryLevel >= 55)
     {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
+        esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000);
 
-    // Main process
-    struct sensor_data results;
-
-    if (mqtt_sensor_bme680_init() == ESP_OK)
-    {
-        if (mqtt_sensor_bme680_get_results_blocking(&results) == ESP_OK)
+        // Initialize NVS
+        esp_err_t ret = nvs_flash_init();
+        if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
         {
-            ESP_LOGI(TAG, "%.3f BME680 Sensor (push): %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm",
-                     (double)sdk_system_get_time() * 1e-3,
-                     results.temperature, results.humidity,
-                     results.pressure, results.gasResistance);
-            mqtt_sensor_data_push(&results);
-
-            if (mqtt_sensor_wifi_connect_to_sta(&wifi_config) == ESP_OK)
-            {
-                mqtt_sensor_sntp_sync();
-
-                if (mqtt_sensor_mqtt_connect(&mqtt_cfg, mqtt_pub_topic) == ESP_OK)
-                {
-                    uint32_t current_buffer_index = mqtt_sensor_data_count();
-                    ESP_LOGI(TAG, "current_buffer_index %d.", current_buffer_index);
-
-                    // Transmit only 50 results to avoid big delays in acquiring the sensor data
-                    if (current_buffer_index > 50)
-                    {
-                        current_buffer_index = 50;
-                    }
-
-                    for (uint32_t i = 0; i < current_buffer_index; i++)
-                    {
-                        mqtt_sensor_data_get_last(&results);
-                        if (mqtt_sensor_mqtt_publish_result(&results, batteryLevel) == ESP_OK)
-                        {
-                            mqtt_sensor_data_drop();
-                        }
-                        else
-                        {
-                            ESP_LOGI(TAG, "mqtt_sensor_mqtt_publish failed - will retry next iteration.");
-                        }
-                    }
-                }
-                mqtt_sensor_mqtt_disconnect();
-            }
-            mqtt_sensor_wifi_disconnect_to_sta();
+            ESP_ERROR_CHECK(nvs_flash_erase());
+            ret = nvs_flash_init();
         }
+        ESP_ERROR_CHECK(ret);
+
+        // Main process
+        struct sensor_data results;
+
+        if (mqtt_sensor_bme680_init() == ESP_OK)
+        {
+            if (mqtt_sensor_bme680_get_results_blocking(&results) == ESP_OK)
+            {
+                ESP_LOGI(TAG, "%.3f BME680 Sensor (push): %.2f °C, %.2f %%, %.2f hPa, %.2f Ohm",
+                         (double)sdk_system_get_time() * 1e-3,
+                         results.temperature, results.humidity,
+                         results.pressure, results.gasResistance);
+                mqtt_sensor_data_push(&results);
+
+                if (mqtt_sensor_wifi_connect_to_sta(&wifi_config) == ESP_OK)
+                {
+                    mqtt_sensor_sntp_sync();
+
+                    if (mqtt_sensor_mqtt_connect(&mqtt_cfg, mqtt_pub_topic) == ESP_OK)
+                    {
+                        uint32_t current_buffer_index = mqtt_sensor_data_count();
+                        ESP_LOGI(TAG, "current_buffer_index %d.", current_buffer_index);
+
+                        // Transmit only 50 results to avoid big delays in acquiring the sensor data
+                        if (current_buffer_index > 50)
+                        {
+                            current_buffer_index = 50;
+                        }
+
+                        for (uint32_t i = 0; i < current_buffer_index; i++)
+                        {
+                            mqtt_sensor_data_get_last(&results);
+                            if (mqtt_sensor_mqtt_publish_result(&results, batteryLevel) == ESP_OK)
+                            {
+                                mqtt_sensor_data_drop();
+                            }
+                            else
+                            {
+                                ESP_LOGI(TAG, "mqtt_sensor_mqtt_publish failed - will retry next iteration.");
+                            }
+                        }
+                    }
+                    mqtt_sensor_mqtt_disconnect();
+                }
+                mqtt_sensor_wifi_disconnect_to_sta();
+            }
+        }
+    }
+    else
+    {
+        // if teh battery level is low it will goto sleep for a
+        // longer time to save battery
+        esp_sleep_enable_timer_wakeup(wakeup_time_sec * 5 * 1000000);
     }
 
 // Code executed when leaving app_main
